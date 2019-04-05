2d60969add2f69fa55012fd1319e67a9ee1ee2ed
Maxim Akimov <akimovmax@mail.ru>
2019-04-05 17:05:13 +0300

    render mock films

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import Film from './film.js';
 import FilmDetails from './film-details.js';
 import Filter from './filter.js';
 
-const film = downloaded.films.all[0];
+console.log(downloaded.films.all)
 
 const filtersContainer = document.querySelector(`.main-navigation`);
 const allFilmsContainer = document.querySelector(`.films-list .films-list__container`);
@@ -16,38 +16,47 @@ const filters = downloaded.filters.map((item) => {
   return item;
 });
 
-const firstFilm = new Film(film);
-const firstFilmDetails = new FilmDetails(film);
-
 filters.forEach((item) => filtersContainer.appendChild(item.render()));
-allFilmsContainer.appendChild(firstFilm.render());
 
-firstFilm.onComments = () => {
-  firstFilmDetails.render();
-  body.appendChild(firstFilmDetails.element);
+const renderAllFilms = (films) => {
+  allFilmsContainer.innerHTML = ``;
+
+  for (const film of films) {
+    const filmComponent = new Film(film);
+    const filmDetailsComponent = new FilmDetails(film);
+
+    filmComponent.onComments = () => {
+      filmDetailsComponent.render();
+      body.appendChild(filmDetailsComponent.element);
+    };
+
+    filmComponent.onAddToWatchList = () => {
+      film.isOnWatchList = !film.isOnWatchList;
+      filmDetailsComponent.update(film);
+    };
+
+    filmComponent.onMarkAsWatched = () => {
+      film.isWatched = !film.isWatched;
+      filmDetailsComponent.update(film);
+    };
+
+    filmComponent.onMarkAsFavorite = () => {
+      film.isFavorite = !film.isFavorite;
+      filmDetailsComponent.update(film);
+    };
+
+    filmDetailsComponent.onClose = (newObject) => {
+      film.rating.user = newObject.rating.user;
+      film.isOnWatchList = newObject.isOnWatchList;
+      film.isWatched = newObject.isWatched;
+      film.isFavorite = newObject.isFavorite;
+      filmDetailsComponent.update(film);
+      body.removeChild(filmDetailsComponent.element);
+      filmDetailsComponent.unrender();
+    };
+
+    allFilmsContainer.appendChild(filmComponent.render());
+  }
 };
 
-firstFilm.onAddToWatchList = () => {
-  film.isOnWatchList = !film.isOnWatchList;
-  firstFilmDetails.update(film);
-};
-
-firstFilm.onMarkAsWatched = () => {
-  film.isWatched = !film.isWatched;
-  firstFilmDetails.update(film);
-};
-
-firstFilm.onMarkAsFavorite = () => {
-  film.isFavorite = !film.isFavorite;
-  firstFilmDetails.update(film);
-};
-
-firstFilmDetails.onClose = (newObject) => {
-  film.rating.user = newObject.rating.user;
-  film.isOnWatchList = newObject.isOnWatchList;
-  film.isWatched = newObject.isWatched;
-  film.isFavorite = newObject.isFavorite;
-  firstFilmDetails.update(film);
-  body.removeChild(firstFilmDetails.element);
-  firstFilmDetails.unrender();
-};
+renderAllFilms(downloaded.films.all);
